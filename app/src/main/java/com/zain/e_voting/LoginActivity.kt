7cc1b253ee2bd720cd41fdd331b08c0a0e1030dc
zainalbani <zainalbani007@gmail.com>
2024-03-01 09:37:05 +0700

    feat: layout main

--- app/src/main/java/com/zain/e_voting/LoginActivity.kt
+++ app/src/main/java/com/zain/e_voting/LoginActivity.kt
@@ -2,10 +2,26 @@ package com.zain.e_voting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.zain.e_voting.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+class LoginActivity : BottomSheetDialogFragment() {
+
+    private var _binding: ActivityLoginBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = ActivityLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
