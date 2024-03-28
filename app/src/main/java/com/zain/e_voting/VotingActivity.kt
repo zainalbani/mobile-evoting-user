a89d6880aa1b5350736dc6cc98421230c9c6d837
zainalbani <zainalbani007@gmail.com>
2024-03-28 13:45:49 +0700

    feat: layout confirmation voting

--- app/src/main/java/com/zain/e_voting/VotingActivity.kt
+++ app/src/main/java/com/zain/e_voting/VotingActivity.kt
@@ -2,10 +2,19 @@ package com.zain.e_voting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zain.e_voting.databinding.ActivityMainBinding
+import com.zain.e_voting.databinding.ActivityVotingBinding
 
 class VotingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityVotingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_voting)
+        binding = ActivityVotingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSubmit.setOnClickListener {
+            val dialogFragment = FixVotingFragment()
+            dialogFragment.show(supportFragmentManager, "dialog_fix")
+        }
     }
 }
